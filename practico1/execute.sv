5630bc9e43d8f105a4b09790b38b5a2ea97f5ac3
irmrz <ignaciotramirez02@gmail.com>
2023-09-05 23:00:04 -0300

    execute agregado

---
+++ practico1/execute.sv
@@ -0,0 +1,13 @@
+module execute (input logic 			 AluSrc,
+					 input logic [3:0]    AluControl,
+					 input logic [63:0]	 PC_E,
+												 signlmm_E,
+												 readData1_E,
+												 readData2_E,
+					 output logic [63:0]  PCBranch_E,
+												 aluResult_E,
+												 writeData_E,
+					 output logic 			 zero_E);
+
+	
+endmodule					 
--- practico1/fetch.sv
+++ practico1/fetch.sv
@@ -6,7 +6,7 @@ module fetch (input  logic 		  PCSrc_F,
 	
 	logic [63:0] salida_mux, salida_adder;
 	initial
-		salida_adder = 0;
+		salida_adder = 64'd4;
 	mux2 mux2_1 (salida_adder, PCBranch_F, PCSrc_F, salida_mux);
 	flopr flipflop (clk, reset, salida_mux, imem_addr_F);
 	adder fetch_adder (imem_addr_F, 64'd4, salida_adder);
