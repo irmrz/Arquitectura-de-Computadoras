b0a0945b0106777d0d2854810f9a253430369ba7
irmrz <ignaciotramirez@mi.unc.edu.ar>
2023-09-06 09:18:25 -0300

    execute hecho

--- practico1/execute.sv
+++ practico1/execute.sv
@@ -8,6 +8,12 @@ module execute (input logic 			 AluSrc,
 												 aluResult_E,
 												 writeData_E,
 					 output logic 			 zero_E);
-
 	
+	logic [63:0] salida_mux, salida_shift_left;
+	
+	mux2 mux_exec(readData2_E, signlmm_E, AluSrc, salida_mux);
+	sl2 shift_left(signlmm_E, salida_shift_left);
+	adder adder_exec(PC_E, salida_shift_left, PCBranch_E);
+	alu alu_exec(readData1_E, salida_mux, AluControl, aluResult_E, zero_E);
+	assign writeData_E = readData2_E;
 endmodule					 
